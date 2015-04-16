8009ed1c78ed3b1c622605ce7125e6d163de1a43
andrewLA331 <andrew.allen2@yahoo.com>
2015-04-16 12:00:08 -0400

    Update ListArrayListBased.java

--- ListArrayListBased.java
+++ ListArrayListBased.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+//comment
 // ****************************************************
 // Array list-based implementation of ADT list.
 // ****************************************************
